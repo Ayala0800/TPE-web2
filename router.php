96b76af528f9b41e932024a636b5031c51decca8
Agustina Quinteros <142256460+Agustinaqui@users.noreply.github.com>
2023-10-17 12:06:36 -0300

    Actualizo modificaciones

---
+++ router.php
@@ -0,0 +1,131 @@
+<?php
+require_once "./app/controller/beer.controller.php";
+require_once "./app/controller/estilo.controller.php";
+require_once "./app/controller/usuario.controller.php";
+
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+if(!empty($_GET['action']))
+    $action = $_GET['action'];
+else
+    $action = 'home';
+
+$params = explode('/', $action);
+$userController = new UserController();
+$cervezaController = new BeerController();
+$estiloController = new EstiloController();
+
+switch ($params[0]) {
+    case 'home':
+        $estilos = $estiloController->getEstilos();
+        $cervezaController->showCervezas($estilos);
+        break; 
+
+    case 'logIn':
+        $userController->showLogIn();
+        break;
+
+    case 'startSession':
+        $userController->startSession();
+        break;
+
+    case 'logOut':
+        $userController->logOut();
+        break;
+
+    case 'modifyUserRol':
+        $userController->modifyUserRol($params[1]);
+        break;
+
+    case 'register':
+        $userController->showRegister();
+    break;
+
+    //AGREGA USUARIO A LA BD
+    case 'addUser':
+        $userController->addUser();
+    break;
+
+    case 'notAdmin':
+        $userController->showError("Se necesitan privilegios de administrador");
+    break;
+
+    case 'notAdmin':
+        $mensaje = "Se necesitan privilegios de administrador";
+        $userController->showError($mensaje);
+    break;
+
+    case 'estilos':
+        $estiloController->showEstilos();
+        break;
+
+    case 'estilo':
+        $estilos = $estiloController->getEstilos();
+        $cervezaController->showCervezaPorEstilo($params[1], $estilos);
+        break;
+
+    //AGREGA EL ESTILO A LA BD
+    case 'addEstilo':
+        $estiloController->addEstilo();
+    break;
+
+    //ELIMINA EL ESTILO DE LA BD
+    case 'deleteEstilo':
+        $estiloController->deleteEstilo($params[1]);
+    break;
+
+    //REDIRIGE AL PHTML PARA EDITAR EL ESTILO QUE SE SELECCIONO
+    case 'editEstilo':
+        $estiloController->showEditEstilo($params[1]);
+    break;
+
+    //ACTUALIZA LOS CAMPOS DE LOS ESTILOS EN LA BD
+    case 'updateEstilo':
+        $estiloController->updateEstilo($params[1]);
+    break;
+
+    case 'cervezas':
+        $estilos = $estiloController->getEstilos();
+        $cervezaController->showCervezas($estilos);
+        break;
+
+    case 'cerveza':
+        $cervezaController->showCerveza($params[1]);
+        break;
+
+    //AGREGA UNA CERVEZA A LA BD
+    case 'addCerveza':
+        $cervezaController->addCerveza();
+    break;
+
+    //ELIMINA UNA CERVEZA DE LA BD
+    case 'deleteCerveza':
+        $cervezaController->deleteCerveza($params[1]);
+    break;
+
+    //REDIRIGE AL PHTML PARA EDITAR LA CERVEZA SELECCIONADA
+    case 'editCerveza':
+        $estilos = $estiloController->getEstilos();
+        $cervezaController->showEditCerveza($params[1], $estilos);
+    break;
+
+    //ACTUALIZA LOS CAMPOS DE LA CERVEZA EN LA BD
+    case 'updateCerveza':
+        $cervezaController->updateCerveza($params[1]);
+    break;
+
+    //PAGINA USUARIOS
+    case 'users':
+        $userController->showUsers();
+    break;
+
+    //ELIMINAR USUARIO EN LA DB
+    case 'deleteUser':
+        $userController->deleteUser($params[1]);
+    break;
+
+    default:
+        echo "404 page not found";
+    break;
+}
